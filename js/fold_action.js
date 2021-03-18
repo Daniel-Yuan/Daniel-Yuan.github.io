67a068c7a59c72a95b182409ae0c77eb92d20881
Daniel-yuan <576311436@qq.com>
2021-03-18 16:56:43 +0800

    Site updated: 2021-03-18 16:56:42

---
+++ js/fold_action.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
+<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
